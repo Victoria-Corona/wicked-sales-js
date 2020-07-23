b9124342bb470583626d1724701e903c17d3a4bb
VicCoro <corona.victoria@hotmail.com>
2020-07-23 16:59:21 -0700

    endpoint returns desired columns

--- server/index.js
+++ server/index.js
@@ -19,24 +19,13 @@ app.get('/api/health-check', (req, res, next) => {
     .catch(err => next(err));
 });
 
-app.use('/api', (req, res, next) => {
-  next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
-});
-
-app.use((err, req, res, next) => {
-  if (err instanceof ClientError) {
-    res.status(err.status).json({ error: err.message });
-  } else {
-    console.error(err);
-    res.status(500).json({
-      error: 'an unexpected error occurred'
-    });
-  }
-});
-
 app.get('/api/products', (req, res, next) => {
   const sql = `
-    select *
+    select "image",
+           "name",
+           "price",
+           "productId",
+           "shortDescription"
       from "products"
   `;
 
@@ -51,6 +40,21 @@ app.get('/api/products', (req, res, next) => {
         error: 'An unexpected error occured.'
       });
     });
+});
+
+app.use('/api', (req, res, next) => {
+  next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
+});
+
+app.use((err, req, res, next) => {
+  if (err instanceof ClientError) {
+    res.status(err.status).json({ error: err.message });
+  } else {
+    console.error(err);
+    res.status(500).json({
+      error: 'an unexpected error occurred'
+    });
+  }
 });
 
 app.listen(process.env.PORT, () => {
