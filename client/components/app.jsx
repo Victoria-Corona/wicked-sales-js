93328c24675f3f4a3f73e85bae21d4dcb13825a6
VicCoro <corona.victoria@hotmail.com>
2020-07-23 17:49:30 -0700

    defined a App class omponent, no initial state, created a header

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PageTitle from './Header';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -18,8 +19,13 @@ export default class App extends React.Component {
   }
 
   render() {
-    return this.state.isLoading
+    return (
+      <>
+      this.state.isLoading
       ? <h1>Testing connections...</h1>
-      : <h1>{this.state.message}</h1>;
+      : <h1>{this.state.message}</h1>
+        <PageTitle text="Wicked Sales" />
+      </>
+    );
   }
 }
