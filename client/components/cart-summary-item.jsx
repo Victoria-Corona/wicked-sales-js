dc66ca0c3227f895a330b39198e25c3dae256520
VicCoro <corona.victoria@hotmail.com>
2020-08-25 17:12:13 -0700

    CartItems preview second pass

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -2,7 +2,21 @@ import React from 'react';
 
 function CartSummaryItem(props) {
   return (
-    <div>{props.cartItemId}</div>
+    <>
+      <div className="card p-4 m-4">
+        <div className="row d-flex justify-content-center">
+          <div className="col-5">
+            <img className="img-fluid preview" src={props.img}></img>
+          </div>
+          <div className="col-7">
+            <div>{props.name}</div>
+            <div>{props.price}</div>
+            <div>{props.shortDesc}</div>
+          </div>
+        </div>
+
+      </div>
+    </>
   );
 }
 
--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -5,15 +5,30 @@ function CartSummary(props) {
   const listOfItems = props.cart.map(item => {
     return (
       <CartSummaryItem key={item.cartItemId}
-        cartItemId={item.cartItemId}/>
+        name={item.name}
+        img={item.image}
+        price={item.price}
+        shortDesc={item.shortDescription}
+        setView={props.setView}/>
     );
   });
   return (
     <>
-      <div>
-        {listOfItems}
-      hi
+      <div className="container">
+        <div className="row">
+          <div className="ml-5 mt-3 p-2">
+            <p className="muted m-0 pointer" onClick={() => props.setView('catalog', {})}><i className="fas fa-chevron-left"></i> Back to catalog</p>
+          </div>
+        </div>
+        <h2 className="pl-3 m-3">My Cart</h2>
+        <div>
+          {listOfItems}
+        </div>
+        <div>
+          <p>Total</p>
+        </div>
       </div>
+
     </>
   );
 }
