2336fa289cb72c03f8de029ed9788a3301d92a2d
VicCoro <corona.victoria@hotmail.com>
2020-08-29 16:33:22 -0700

    further styling for checkoutForm, app renders checkoutForm from setView

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -11,28 +11,39 @@ export default class CheckoutForm extends React.Component {
   }
 
   render() {
+
+    const price = this.props.cart;
+    const sum = price.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue.price;
+    }, 0);
+
+    const totalAdjust = <span>&#36;{(sum / 100).toFixed(2)}</span>;
+
     return (
-      <div>
-        <div>
-          <h1>My Cart</h1>
-          <p className="muted">Order Total:</p>
+      <div className="ml-5 container">
+        <div className="pl-3 mt-3">
+          <h2>My Cart</h2>
+          <p className="muted mt-3">Order Total: {totalAdjust}</p>
         </div>
         <form>
           <div className="form-group">
-            <div className="mt-3 p-3">
-              <label htmlFor="">Name</label>
+            <div className="pl-3 mt-3">
+              <label className="pl-2" htmlFor="">Name</label>
               <input type="text" name="name" className="form-control" placeholder="Jane Doe"/>
             </div>
-            <div className="mt-3 p-3">
-              <label htmlFor="">Credit Card</label>
+            <div className="pl-3 mt-3">
+              <label className="pl-2" htmlFor="">Credit Card</label>
               <input type="text" name="creditCard" className="form-control" placeholder="0000 0000 0000 0000"/>
             </div>
-            <div className="mt-3 p-3">
-              <label htmlFor="">Shipping Address</label>
-              <textarea type="text" name="shippingAddress" className="form-control" placeholder="Jane Doe"></textarea>
+            <div className="pl-3 mt-3">
+              <label className="pl-2" htmlFor="">Shipping Address</label>
+              <textarea type="text" name="shippingAddress" className="form-control" placeholder="123 LearningFuze"></textarea>
             </div>
           </div>
-          <button type="button" className="btn btn-primary">Place Order</button>
+          <div className="d-flex justify-content-between">
+            <p className="muted m-0 pointer pl-3" onClick={() => this.props.setView('catalog', {})}><i className="fas fa-chevron-left"></i> Back to catalog</p>
+            <button type="button" className="btn btn-primary ml-3 mt-3">Place Order</button>
+          </div>
         </form>
       </div>
     );
