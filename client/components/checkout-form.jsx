b29097485dbf4fbfd4be5d59eba637289997bd91
VicCoro <corona.victoria@hotmail.com>
2020-11-02 21:40:09 -0800

    moved modal, added error styling, second pass on form validation

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -6,7 +6,8 @@ export default class CheckoutForm extends React.Component {
     this.state = {
       name: '',
       creditCard: '',
-      shippingAddress: ''
+      shippingAddress: '',
+      isFilled: ''
     };
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -22,9 +23,15 @@ export default class CheckoutForm extends React.Component {
 
   handleSubmit(event) {
     if (this.state.name === '' || this.state.creditCard === '' || this.state.shippingAddress === '') {
-      alert('Worked');
+      this.setState({
+        isFilled: false
+      });
+      this.unfulfilled();
       event.preventDefault();
     } else {
+      this.setState({
+        isFilled: true
+      });
       event.preventDefault();
       const orderInfo = {
         name: this.state.name,
@@ -36,13 +43,23 @@ export default class CheckoutForm extends React.Component {
     }
   }
 
+  unfulfilled() {
+    if (this.state.isFilled === false) {
+      return 'red';
+    } else {
+      return '';
+    }
+
+  }
+
   render() {
     const price = this.props.cart;
     const sum = price.reduce((accumulator, currentValue) => {
       return accumulator + currentValue.price;
     }, 0);
-
     const totalAdjust = <span>&#36;{(sum / 100).toFixed(2)}</span>;
+    const validate = this.unfulfilled();
+    const formValidation = `form-control ${validate}`;
 
     return (
       <div className="mt-5 container body-text">
@@ -54,15 +71,15 @@ export default class CheckoutForm extends React.Component {
           <div className="form-group">
             <div className="pl-3 mt-3">
               <label className="pl-2" htmlFor="">Name</label>
-              <input type="text" name="name" className="form-control" placeholder="Jane Doe" onChange={this.handleInputChange}/>
+              <input type="text" name="name" className={formValidation} placeholder="Jane Doe" onChange={this.handleInputChange}/>
             </div>
             <div className="pl-3 mt-3">
               <label className="pl-2" htmlFor="">Credit Card</label>
-              <input type="text" name="creditCard" className="form-control" placeholder="0000 0000 0000 0000" onChange={this.handleInputChange}/>
+              <input type="text" name="creditCard" className={formValidation} placeholder="0000 0000 0000 0000" onChange={this.handleInputChange}/>
             </div>
             <div className="pl-3 mt-3">
               <label className="pl-2" htmlFor="">Shipping Address</label>
-              <textarea type="text" name="shippingAddress" className="form-control" placeholder="123 LearningFuze" onChange={this.handleInputChange}></textarea>
+              <textarea type="text" name="shippingAddress" className={formValidation} placeholder="123 LearningFuze" onChange={this.handleInputChange}></textarea>
             </div>
           </div>
           <div className="d-flex justify-content-between mt-4">
